1e48b1c4061bb70a284cedd81cb8f7b0cae2d5dd
Christian Gomez <cjgm14@gmail.com>
2020-02-14 23:19:58 -0600

    estilos agregados

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import RecipesGenerator from './RecipesGenerator';
 
 const App = () => (
-  <>
+  <main className="container-fluid d-flex flex-column justify-content-center align-items-center">
     <RecipesGenerator />
-  </>
+  </main>
 );
 
 export default App;
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,33 +1,56 @@
 import React from 'react';
 
-const Recipe = props => (
-  <>
-    <section>
-      <div className="container-fluid">
-        <div className="row">
-          <picture className="col-md-6">
-            <img src={props.recipe.mealNameThumb} alt="..." className="card-img-top img-fluid" />
-          </picture>
-          <div className="col-md-6">
-            <h2>{props.recipe.mealName}</h2>
-            <p>{props.recipe.instructions}</p>
+const Recipe = props => {
+  return (
+    <>
+      <section className="mt-5">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <img
+                src={props.recipe.mealNameThumb}
+                alt="Imagen no encontrada"
+                className="card-img-top img-fluid"
+                style={{ maxHeight: '350px' }}
+              />
+            </div>
+            <div className="col-md-8">
+              <h2>{props.recipe.mealName}</h2>
+              <p>{props.recipe.instructions}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <p>Category: {props.recipe.mealCategory}</p>
-      <p>Area: American</p>
-      <p>Targs: Breakfast, desert, sweet, fruity</p>
-      <h3>Ingredients</h3>
-      <ul>
-        <li>
-          {props.recipe.ingredient1} - {props.recipe.measure1}
-        </li>
-        <li>
-          {props.recipe.ingredient2} - {props.recipe.measure2}
-        </li>
-      </ul>
-    </section>
-  </>
-);
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <p className="h2 mt-4">Category: {props.recipe.mealCategory}</p>
+              <h3 className="mt-4 mb-3">Ingredients</h3>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  {props.recipe.ingredient1} - {props.recipe.measure1}
+                </li>
+                <li className="list-group-item">
+                  {props.recipe.ingredient2} - {props.recipe.measure2}
+                </li>
+                <li className="list-group-item">
+                  {props.recipe.ingredient3} - {props.recipe.measure3}
+                </li>
+                <li className="list-group-item">
+                  {props.recipe.ingredient4} - {props.recipe.measure4}
+                </li>
+                <li className="list-group-item">
+                  {props.recipe.ingredient5} - {props.recipe.measure5}
+                </li>
+                <li className="list-group-item">
+                  {props.recipe.ingredient6} - {props.recipe.measure6}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Recipe;
